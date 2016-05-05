e1c5adba9ee0ef8422f1f807d3047bdb71c339f1
Egor Kloos <egor+git@lunatech.com>
2016-05-05 16:12:36 +0200

    minor improvement for readability and dryness.

--- throttleEvent.js
+++ throttleEvent.js
@@ -7,25 +7,23 @@
  * @arg {number} ms     - The optional delay time in milliseconds (throttling)
  * @return {function}   - The timeout function that is triggered by the event listner on each event call
  */
-export let throttleEvent = (func, ms) => {
+export throttleEvent = (func, ms) => {
   const DELAY = ms || 250;
   var timestamp;
   var timeout;
   return event => {
     var currentEventTime = +new Date;
-    if (timestamp && currentEventTime < timestamp + DELAY) {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => {
-        timestamp = currentEventTime;
-        requestAnimationFrame(() => {
-          func(event)
-        });
-      }, DELAY);
-    } else {
+    var invokeFunc = () => {
       timestamp = currentEventTime;
       requestAnimationFrame(() => {
         func(event)
       });
+    }
+    if (timestamp && currentEventTime < timestamp + DELAY) {
+      clearTimeout(timeout);
+      timeout = setTimeout(invokeFunc, DELAY);
+    } else {
+      invokeFunc();
     }
   };
 }
